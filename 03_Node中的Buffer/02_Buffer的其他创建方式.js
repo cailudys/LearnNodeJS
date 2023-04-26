a02b53289eff6a8199a62366edc7375f8916074b
cailudys <cailudys@gmail.com>
2023-04-26 18:44:45 +0800

    buffer的其他创建方式

---
+++ 03_Node中的Buffer/02_Buffer的其他创建方式.js
@@ -0,0 +1,16 @@
+const buf = Buffer.alloc(8);
+console.log(buf);
+
+// 2.手动对每个字节进行访问
+console.log(buf[0]);
+console.log(buf[1]);
+
+// 3.手动对每个字节进行操作
+buf[0] = 100;
+buf[1] = 0x66;
+
+console.log(buf.toString());
+
+buf[2] = "m".charCodeAt();
+
+console.log(buf);
