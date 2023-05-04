4a1d1a481492dfd2020bad35b193c7ee5e082ce1
cailudys <cailudys@gmail.com>
2023-05-04 14:40:10 +0800

    文件上传

--- 05_Node服务器-http模块/16_文件上传-正确的方法.js
+++ 05_Node服务器-http模块/16_文件上传-正确的方法.js
@@ -21,7 +21,6 @@ const server = http.createServer((req, res) => {
     //1. 截取从image/jpeg位置开始后面所有的数据
     const imageType = "image/jpeg";
     const imageTypePosition = formData.indexOf(imageType) + imageType.length;
-    console.log("imageTypePosition", imageTypePosition);
     let imageData = formData.substring(imageTypePosition);
 
     //2. imageData开始位置会有两个空格
@@ -30,12 +29,10 @@ const server = http.createServer((req, res) => {
     //3. 替换最后的boundary
     imageData = imageData.substring(0, imageData.indexOf(`--${boundary}--`));
 
-    console.log(imageData);
-
     //4. 将imageData的数据存储到文件中
     fs.writeFile("./bar.jpg", imageData, "binary", () => {
       console.log("文件存储成功");
-      res.end("文件上传完成");
+      res.end("123");
     });
   });
 });
