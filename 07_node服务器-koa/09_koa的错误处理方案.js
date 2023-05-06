53c8e093842ae8a54bb20fe6f44d8c7c6395d3d8
cailudys <cailudys@gmail.com>
2023-05-06 15:36:23 +0800

    koa中错误处理方案

---
+++ 07_node服务器-koa/09_koa的错误处理方案.js
@@ -0,0 +1,40 @@
+const Koa = require("koa");
+const KoaRouter = require("@koa/router");
+// 创建app对象
+const app = new Koa();
+// 创建userRouter对象
+const userRouter = new KoaRouter({ prefix: "/users" });
+
+userRouter.get("/", (ctx, next) => {
+  const isAuth = false;
+  if (isAuth) {
+    ctx.body = "user list data ~";
+  } else {
+    ctx.app.emit("error", -1003, ctx);
+  }
+});
+
+// 让路由里的中间件生效，并且有提升 method not allowed
+app.use(userRouter.routes());
+app.use(userRouter.allowedMethods());
+
+// 统一错误处理 独立的文件 error-hadle.js
+app.on("error", (code, ctx) => {
+  const errorCode = code;
+  let message = "";
+  switch (errorCode) {
+    case -1001:
+      message = "账号或者密码错误~";
+      break;
+    case -1002:
+      message = "请求参数不正确~";
+      break;
+    case -1003:
+      message = "未授权,请检查你的token信息";
+      break;
+  }
+});
+
+app.listen(6000, () => {
+  console.log("服务器启动成功~");
+});
