5f403044c6397dcc6ef045e3a683927e6ca1ea8d
cailudys <cailudys@gmail.com>
2023-05-09 11:36:17 +0800

    mysql2 promis的写法

--- 09_MySQL数据库/05_mysql2-结果promise使用.js
+++ 09_MySQL数据库/05_mysql2-结果promise使用.js
@@ -9,8 +9,14 @@ const connectionPool = mysql.createPool({
   connectionLimit: 5,
 });
 
-// 编写一个预处理语句
 const statement = "SELECT * FROM `products` WHERE price > ? AND score > ?;";
 
-// 执行这个预处理语句
-connectionPool.execute(statement, [1000, 8], (err, values, fields) => {});
+// connectionPool.promise().execute() 这样执行后拿到的结果就是一个promise
+connectionPool
+  .promise()
+  .execute(statement, [1000, 8])
+  .then((res) => {
+    const [values, fields] = res;
+    console.log(values);
+    console.log(fields);
+  });
